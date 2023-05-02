303fa238f5551482c7e788a059acb12e80a7fe10
Aditya Raj Singh <adi4adityaraj@gmail.com>
2023-05-02 14:20:24 +0530

    swagger changes

--- docs/docs.go
+++ docs/docs.go
@@ -231,7 +231,7 @@ const docTemplate = `{
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
 	Version:          "2.0",
-	Host:             "localhost:8000",
+	Host:             "API_URL",
 	BasePath:         "/",
 	Schemes:          []string{"http"},
 	Title:            "Blog-Post API",
